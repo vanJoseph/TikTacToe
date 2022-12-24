2a1da3aea744f803156af4e3a00a7008de7b2b51
Donovan Wilder <donovan.wilder@gmail.com>
2022-12-24 04:45:05 -0500

    Added a way to convert valid symbols to the proper enum

--- src/main/kotlin/Pieces.kt
+++ src/main/kotlin/Pieces.kt
@@ -13,5 +13,13 @@ enum class Pieces {
         override fun toString(): String {
             return "_"
         }
+    };
+    fun getPiece(character: Char): Pieces{
+       when (character){
+           'x','X'-> return PLAYER_X
+           'o','O' -> return PLAYER_O
+           '_' -> return NONE
+           else -> throw InvalidCharacterException()
+       }
     }
 }
