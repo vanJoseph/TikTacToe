95f32607bb60977828705e975ad6808e353b169b
Donovan Wilder <donovan.wilder@gmail.com>
2022-12-24 20:18:43 -0500

    Added test for a clean reset of board

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -2,21 +2,31 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class BoardTest {
-    var board:Board?= null
+    var board: Board? = null
 
     @org.junit.jupiter.api.BeforeEach
     fun setUp() {
-        board=Board()
+        board = Board()
     }
-@Test
-    fun Should_BoardMatch_When_EnteredAsTextInput(){
-    var textBoard= "xxxooox__"
-    var exptected ="X  X  X  \nO  O  O  \nX  _  _  \n"
-    board!!.inputTextBoard(textBoard)
-    var result= board.toString()
-    assertEquals(exptected, result)
-}
 
+    @Test
+    fun Should_BoardMatch_When_EnteredAsTextInput() {
+        var textBoard = "xxxooox__"
+        var exptected = "X  X  X  \nO  O  O  \nX  _  _  \n"
+        board!!.inputTextBoard(textBoard)
+        var result = board.toString()
+        assertEquals(exptected, result)
+    }
+
+    @Test
+    fun Should_Reset(){
+        var fullBoard = "oxoxoxoxo"
+        var exptected = "_  _  _  \n_  _  _  \n_  _  _  \n"
+        board!!.inputTextBoard(fullBoard)
+        var result = board!!.toString()
+        board!!.reset()
+        assertEquals(exptected,result)
+    }
 
     @org.junit.jupiter.api.AfterEach
     fun tearDown() {
