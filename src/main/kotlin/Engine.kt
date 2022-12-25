d4a871f679d7cc9c757532a0daaaafd5f256b45e
Donovan Wilder <donovan.wilder@gmail.com>
2022-12-24 21:44:01 -0500

    Add validation to check that there is only 2 values that are numbers in range

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -1,8 +1,31 @@
-class Engine {
-    private val board= Board()
-    private val playersTurn= Pieces.PLAYER_X
-    private var turnNumber = 0
-    fun startNewGame(){
+import java.lang.NumberFormatException
 
+class Engine {
+    private val board = Board()
+    private val playersTurn = Pieces.PLAYER_X
+    private var turnNumber = 0
+    fun startNewGame() {
+        board.reset()
+        getplayerInput()
+
+    }
+
+    fun getplayerInput(): List<String> {
+        return readln().split(",")
+    }
+
+    fun validateInput(input: List<String>): Boolean {
+        if (input.size != 2)
+            return false
+        for (i in input) {
+            try {
+                val value = i.toInt()
+                if(value>2||value<0)
+                    return false
+            } catch (e: NumberFormatException) {
+                return false
+            }
+        }
+        return true
     }
 }
